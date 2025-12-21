51a17d2846e6548ea642c2c2f259e4d9db49d6e5
Muhammad Ayyub Ramli <azizahrah137@gmail.com>
2025-12-21 13:58:54 +0700

    update router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -7,7 +7,6 @@ api_router = APIRouter()
 
 api_router.include_router(authentication.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(transtrack.router, prefix="/transtrack", tags=["Transtrack Data Summary (Data Integration & Processing)"])
-api_router.include_router(transtrack.router, prefix="/transtrack", tags=["Transtrack Data Summary (Data Integration & Processing)"])
 api_router.include_router(emission.router, prefix="/emission", tags=["Emission Prediction Engine"])
 api_router.include_router(anomaly.router, prefix="/anomaly", tags=["Anomaly Detection Engine"])
 api_router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard & Visualization"])
